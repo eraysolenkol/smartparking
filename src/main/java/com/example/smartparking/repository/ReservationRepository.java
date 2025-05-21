cbc476f14b4dfa248e1559d84159232d5df746de
Eray <solenkoleray@gmail.com>
2025-05-21 19:15:26 +0200

    Basic Reservation Repository Created

---
+++ src/main/java/com/example/smartparking/repository/ReservationRepository.java
@@ -0,0 +1,15 @@
+package com.example.smartparking.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.smartparking.model.Reservation;
+import java.util.List;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    List<Reservation> findByUserId(Long userId);
+
+    List<Reservation> findByParkingLocationId(Long parkingLocationId);
+
+}
